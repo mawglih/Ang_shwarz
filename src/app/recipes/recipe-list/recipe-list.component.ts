19639686c7e82180a75066efcb2d6ac02983005b
Oleg Markoff <mawglih@gmail.com>
2017-08-14 15:47:08 -0400

    first

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Recipe } from '../recipe.model'
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'Test description', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
